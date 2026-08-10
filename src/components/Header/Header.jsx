195fab8d07fa51d8ac3df92dfbf2f91097e546a6
Fabiox7778 <fabiorabello08@gmail.com>
2026-08-10 10:27:04 -0300

    feat: Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,26 @@
-"use client";
-import Link from "next/link";
-export default function Header() {
-    return (
-    <nav>
-        <Link href="/">Home</Link>
-        <Link href="/personagens">Personagens</Link>
-        <Link href="/sobre">Sobre</Link>
-    </nav>
-    );
+import styles from "./Header.module.css";
+import Image from "next/image";
+
+export default function Header({ title, subtitle }) {
+  return (
+    <header className={styles.header}>
+      <div className={styles.brand}>
+        <Image
+          src="/images/logo-potter-next.svg"
+          alt="Minha logo"
+          loading="eager"
+          width={500}
+          height={500}
+          style={{
+            width: '20%',
+            height: 'auto'
+          }}
+        />
+        <div>
+          <h1 className={styles.title}>{title}</h1>
+          <p className={styles.subtitle}>{subtitle}</p>
+        </div>
+      </div>
+    </header>
+  );
 }
